22e8ffdf69901fa0da8cf39d579676ffd3b78fa8
sewata <deep.sea.odyssey@gmail.com>
2013-11-10 16:39:54 +0900

    Update combination.pl

--- combination.pl
+++ combination.pl
@@ -17,3 +17,13 @@ sub combi{
 
 my $ans = combi($ARGV[0],$ARGV[1]);
 print "The answer is $ans\n";
+
+#　組合せの数の計算
+#　白黒赤青の4色の中から2色を選び出す場合、
+#　（白、黒）（白、赤）（白、青）（黒、赤）（黒、青）（赤、青）
+#　C(4,2) = 6
+#　0 < r < nのとき、C(n,r) = C(n-1,r-1) + C(n-1,r)
+#　r=nのとき、C(n,r) = 1
+#　r=0のとき、C(n,r) = 1
+
+#　perl combination.pl n r
